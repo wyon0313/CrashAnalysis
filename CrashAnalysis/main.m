b877aa24cc8fd1ec0803aff92157726e9def8585
王永刚 <wyon.wang@gmail.com>
2018-04-28 18:44:34 +0800

    Initial Commit

---
+++ CrashAnalysis/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrashAnalysis
+//
+//  Created by 王永刚 on 2018/4/28.
+//  Copyright © 2018年 CrashAnalysis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
